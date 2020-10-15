233a288dd53ad2507ab7a1ce3fd7b210b285f363
nicholasscabral <nicholasscabral@gmail.com>
2020-10-15 09:40:16 -0300

    add landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {FiArrowRight} from 'react-icons/fi'
+
+import './styles/global.css'
+import './styles/pages/landing.css'
+
+import logoImg from './images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Fortaleza</strong>
+          <span>Ceará</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </a>
+
+      </div>
     </div>
   );
 }
