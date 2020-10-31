7d5955f2ba178f59ae462e5e322ce484360a45f2
nicholasscabral <nicholasscabral@gmail.com>
2020-10-30 23:32:45 -0300

    setting sidebar as componente and mapIcon

--- web/src/pages/orphanagesMap.tsx
+++ web/src/pages/orphanagesMap.tsx
@@ -1,24 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FiPlus, FiArrowRight } from 'react-icons/fi'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
-import Leaflet from 'leaflet'
-
-import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg'
 
 import '../styles/pages/orphanage-map.css'
-
-const mapIcon = Leaflet.icon({
-    iconUrl: mapMarkerImg,
-
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
+import mapIcon from '../utils/mapIcon'
 
 function OrphanagesMap() {
+
+
     return (
         <div id="page-map">
             <aside>
@@ -50,14 +42,14 @@ function OrphanagesMap() {
                 >
                     <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
                         LAR DAS MENINAS
-                        <Link to="">
+                        <Link to="/orphanages/1">
                             <FiArrowRight size={20} color="#FFF" />
                         </Link>
                     </Popup>
                 </Marker>
             </Map>
 
-            <Link to='' className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#fff" />
             </Link>
         </div>
